726c300846ac5c4dba2d327134dd3c812e594d3e
Kassie Bradshaw <kassie.r.bradshaw@gmail.com>
2022-08-05 13:49:18 -0700

    Class 10 review

---
+++ class-10/review/review.js
@@ -0,0 +1,44 @@
+// for-loops - when you want to loop a specific number of times
+
+// basic structure of a for-loop
+for (let i = 0; i < 10; i++){ // code goes here}
+
+// initial value | condition | increment/decrement
+for (let potato = 0; potato < 10; potato++){
+    console.log(potato);
+    potato++;
+  }
+
+console.log("---------------------------");
+  
+for (let potato = 0; potato < 10; potato++){
+  potato++;
+  console.log(potato);
+}
+
+// while loops - when you don't know how many times the loop will execute
+
+// while (this is true){ execute this code }
+let mySwitch = true;
+console.log(mySwitch);
+while (mySwitch){
+  console.log("Hi class!");
+  mySwitch = false;
+}
+
+//             True                  F
+console.log(( 5 > 4 || 5 < 10) && 5 > 10);
+
+// javascript quick recap
+
+// two ways to define a function / structure of a function
+// function myFunction(parameters){ code to execute };
+// function myFunction(){
+//      code goes here
+//      return statement (to return some value outside of your function - also optional, but a good idea)
+}
+// const myFunction = function(){};
+
+
+//myFunction(arguments);
+
---
+++ class-10/review/review.md
@@ -0,0 +1,8 @@
+repl.it
+codepen.io
+https://convertingcolors.com/
+
+Every course moving forward will have prework
+Give yourself plenty of time to get it done
+
+Go back to old assignments and attempt the stretch goals.
